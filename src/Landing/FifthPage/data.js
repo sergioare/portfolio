b82f65201ee29e46a846d9cf2ff0f6abc5e67493
unknown <sergioarevalo301@gmail.com>
2023-05-29 20:05:58 -0500

    src(Landing/fifhPag): add data.js

--- src/Landing/FifthPage/data.js
+++ src/Landing/FifthPage/data.js
@@ -1,24 +1,40 @@
 export const professionalExp = [
     {
         titleJob: "Full Stack Developer",
-        company:"",
-        tasks: [],
-        durationFrom: "",
-        durationTo: "",
+        company:"Soy Henry",
+        tasks: [
+            "Promoted of design and brand identity through the creation of company logos and the creation of an attractive, fluid, user-friendly, minimalist and unique website (UX/UI).",
+            "Worked with React-redux, NodeJS, Firebase, Figma.",
+            "Worked with back-end team to design APIs.",
+            "Implemented firebase authentication with data persistence.",
+            "Participated in requirements gathering to solidify prerequisites and determine best technical solution to meet business needs.",
+            "Made recommendations for integration of new technologies based on tech targets and alignment with objectives.",
+            "Contributed to projects within Scrum project management environments.",
+            "Performed modals in login, register, shopping cart and forms.",
+            "I was supporting, supervising and leading the Front team to achieve the goals of each Sprint.",
+            "Reviewed code, debugged problems, and corrected issues.",
+            "I wrote the documentation for the Front part.",
+            "Exercised leadership capabilities by successfully motivating and inspiring others."
+        ],
+        durationFrom: "February 2023",
+        durationTo: "March 2023",
     },
     {
         titleJob: "Administrative Assistant",
-        company:"",
-        tasks: [],
-        durationFrom: "",
-        durationTo: "",
+        company:"DTP-M Consortium",
+        tasks: [
+            "Managed about sixty people to achieve the contract objectives.",
+            "Negotiated contracts with vendors and suppliers for office supplies, equipment and services."
+        ],
+        durationFrom: "August 2019",
+        durationTo: "October 2019",
     },
     {
-        titleJob: "Eventech",
-        company:"",
+        titleJob: "",
+        company:"Eventech",
         tasks: [],
-        durationFrom: "",
-        durationTo: "",
+        durationFrom: "May 2017",
+        durationTo: "March 2018",
     },
     {
         titleJob: "Infomática & Tecnología Stefanini",
@@ -30,7 +46,10 @@ export const professionalExp = [
     {
         titleJob: "University Coordinator",
         company:"",
-        tasks: [],
+        tasks: [
+            "Self-motivated, with a strong sense of personal responsibility.",
+            "Demonstrated leadership skills in managing projects."
+        ],
         durationFrom: "",
         durationTo: "",
     },
